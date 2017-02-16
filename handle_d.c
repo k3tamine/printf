cb2bdaf4635751d30fea7547e4cc5fddddfc3ee6
Mathieu GONON <mgonon@e1r7p4.42.fr>
2017-02-16 22:41:12 +0100

    Almost nothing change

--- handle_d.c
+++ handle_d.c
@@ -52,6 +52,28 @@ int	apply_width(int width, int size, int nb, int len)
 	return (width - size);
 }
 
+// //TODO
+// int	apply_width_preci(int width, int size, int nb, int len)
+// {
+// 	char	buf[4096];
+// 	int		ret;
+// 	int		i;
+
+// 	if (size < len)
+// 		size = len;
+// 	i = -1;
+// 	// ret = width - size;
+// 	// if (nb < 0)
+// 		// width--;
+// 	if (width <= size)
+// 		return (0);
+// 	// printf("\nwidth = %d, size = %d, nb = %d\n", width, size, nb);
+// 	while (++i < width - size)
+// 		buf[i] = ' ';
+// 	write(1, buf, width - size);
+// 	return (width - size);
+// }
+
 // int	apply_minus(int nb, int len, char *buf, t_format *frmt)
 // {
 // 	int	add_len;
@@ -76,7 +98,6 @@ int	apply_classic(int nb, int len, char *buf, t_format *frmt)
 	i = 0;
 	add_len = len;
 	add_len += frmt->flags.minus ? 0 : apply_width(frmt->width, frmt->precision, nb, len);
-	// printf("\nICCCCII\n");
 	if (frmt->flags.plus && nb > 0)
 		write(1, "+", 1);
 	else if (nb < 0)
@@ -84,14 +105,16 @@ int	apply_classic(int nb, int len, char *buf, t_format *frmt)
 		write(1, "-", 1);
 		i = 1;
 	}
-	// printf("\n len = %d, frmt->width = %d, nb = %d\n", len, frmt->width, nb);
 	add_len += frmt->flags.minus ? apply_precision(frmt->precision, len, nb) : 0;
-	// printf("\n add_len = %d, frmt->width = %d, nb = %d\n", add_len, frmt->width, nb);
 	write(1, buf + i, len - i);
 	add_len += frmt->flags.minus ? apply_width(frmt->width, add_len, nb, len) : 
 		apply_precision(frmt->precision, len, nb);
 	return (add_len);
+	// printf("\n len = %d, frmt->width = %d, nb = %d\n", len, frmt->width, nb);
+	// printf("\n add_len = %d, frmt->width = %d, nb = %d\n", add_len, frmt->width, nb);
 }
+
+int	flag_zero()
 
 int	handle_d(int nb, char *buf, t_format *frmt)
 {
@@ -104,7 +127,7 @@ int	handle_d(int nb, char *buf, t_format *frmt)
 	// if (frmt->flags.minus)
 	// 	len += apply_minus(nb, len, buf, frmt);
 	// else
-		len += apply_classic(nb, len, buf, frmt);
+	len += apply_classic(nb, len, buf, frmt);
 	return (len);
 }
 
